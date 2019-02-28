cb3d139a9ca7f9f4fa5911a1adac75990007e94e
Edu <eramu_92@hotmail.com>
2019-02-28 16:52:51 +0100

    second commit

--- src/proyectOpt/Main.java
+++ src/proyectOpt/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		int y = 100;
-		System.out.println(y);		
+		System.out.println(y);
+		int x = 20;
+		System.out.println(x);
 		
 	}
 
