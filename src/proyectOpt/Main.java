a5496ffd1072cbf5a058adb2fb5190ff08cac245
Edu <eramu_92@hotmail.com>
2019-02-28 17:16:00 +0100

    branch changes

--- src/proyectOpt/Main.java
+++ src/proyectOpt/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		System.out.println(y);
 		int x = 20;
 		System.out.println(x);
+		//Modificaciones para branch_1
+		String text = "Mas pruebas";
+		System.out.println(text);
 		
 	}
 
